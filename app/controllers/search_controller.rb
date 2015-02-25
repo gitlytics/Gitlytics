853131bcba2d6b1f577745cac94f1517af2e1dc2
BrianNewsom <brian.newsom@colorado.edu>
2015-02-25 00:53:17 -0700

    Add additional queries and simple auth based on env variables

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,6 +1,41 @@
+# TODO: Where should globals go?
+USERNAME = ENV['GH_USERNAME']
+PASSWORD = ENV['GH_PASSWORD']
+
 class SearchController < ApplicationController
   def index
-    @repo = Github.repos.get user: params[:user], repo: params[:repo]
+    github = Github.new basic_auth: "#{USERNAME}:#{PASSWORD}"
+    @repo = github.repos.get user: params[:user], repo: params[:repo]
+    @languages = github.repos.languages user: params[:user], repo: params[:repo]
+    @pulls = getAllPulls github
+  
   end
+  
+  private
 
+  def getAllPulls github
+    # I believe this fn should go in the github class
+    pulls = Array.new
+    counter = 1
+    # As far as I can tell, we can't get the # of pulls, so we have to go until we hit one that doesn't exist
+    # TODO: Find a nicer way to do this.
+    while true do
+      # Add error handling for when we hit the last PR
+      begin
+        pull = github.pull_requests.get user: params[:user], repo: params[:repo], number: counter
+      rescue Github::Error::GithubError => e
+        puts e
+        break
+      end
+      if pull
+        # We'll probably actually want all (or most) of the data here rather than just state
+        pull = pull.state
+        pulls << pull
+        counter += 1
+      else
+        break
+      end
+    end
+    return pulls
+  end
 end
