6d3cffca4853b60bddfd0ce2890fd28a07f0fb6e
BrianNewsom <brian.newsom@colorado.edu>
2015-03-29 12:47:52 -0600

    reenable error pages and make try catch only catch errors on first get call

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -9,19 +9,21 @@ class SearchController < ApplicationController
 
   def index
     # Create analytics page
+    # Only redirect if get repo call fails.  If others fail, let it fail so we can troubleshoot.
     begin
       # Check for errors in case repo doesn't exist
       @repo = GithubClient.get params[:user], params[:repo]
+    rescue
+      # If error -> redirect to error page w/ relevant info, end fn w/ return
+      puts "Repo not found"
+      redirect_to :controller => 'errors', :action => 'bad_repo', :user => params[:user], :repo => params[:repo]
+      return
+    end
       @contrib = GithubClient.getContrib params[:user], params[:repo]
       @languages = GithubClient.getLanguages params[:user], params[:repo]
       @pulls = GithubClient.getPulls params[:user], params[:repo]
       @read = GithubClient.getReadMe params[:user], params[:repo]
       @repoList = GithubClient.getRepos params[:user]
-    rescue
-      # If error -> redirect to error page w/ relevant info
-      puts "Repo not found"
-      redirect_to :controller => 'errors', :action => 'bad_repo', :user => params[:user], :repo => params[:repo]
-    end
   end
 
 
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -10,7 +10,8 @@ Rails.application.configure do
   config.eager_load = false
 
   # Show full error reports and disable caching.
-  config.consider_all_requests_local       = false
+  # TODO: set to false to see custom error pages rather than ruby helpers
+  config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
