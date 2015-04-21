7210a4c671d62bb4b4dc87a4fb8c0465c8a4985c
Nico Tonozzi <nico@nicot.us>
2015-04-21 13:25:11 -0600

    db migration

--- db/migrate/20150421191955_new.rb
+++ db/migrate/20150421191955_new.rb
@@ -1,4 +1,18 @@
 class New < ActiveRecord::Migration
+  enable_extension 'hstore'
   def change
+    create_table :repositories do |t|
+      t.text :user
+      t.text :repo_name
+      t.text :refreshed
+      t.text :stargazers
+      t.text :lang
+      t.text :percentage_pulls_merged,
+      t.text :contributors
+      t.text :pulls_to_issues
+      t.text :readme
+      t.text :popularity
+      t.text :growth_rate
+      t.hstore :languages
   end
 end
